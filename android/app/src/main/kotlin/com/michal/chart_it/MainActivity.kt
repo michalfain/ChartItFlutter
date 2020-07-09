86dab0569bc66feb2af1c8dc8cae93e5fd164dd1
michal fain <michelle.fain@gmail.com>
2020-07-09 12:31:55 +0300

    inital commit

---
+++ android/app/src/main/kotlin/com/michal/chart_it/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.michal.chart_it
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,26 @@ import 'package:chart_it/widgets/ChordWidget.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Chart it"),),
+        appBar: AppBar(
+          title: Text("Chart it"),
+        ),
         body: Column(
           children: [
             GetChord(),
+            Container(
+              child: Text('hi'),
+            )
           ],
         ),
       ),
